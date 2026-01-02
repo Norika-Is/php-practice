7a0da56ffcfe074b822d60489833ffe91b38e69e
gami-g-2 <norika0127aaa@gmail.com>
2026-01-03 01:07:01 +0900

    feat: php-practice-1

--- php-practice_1.php
+++ php-practice_1.php
@@ -1,41 +1,148 @@
 <?php
 // Q1 変数と文字列
-
+$name = '石神';
+echo '私の名前は「' . $name . '」です。';
 
 // Q2 四則演算
-
+$num = 5 * 4;
+echo $num . "\n";
+echo $num / 2;
 
 // Q3 日付操作
-
+echo '現在時刻は、' . date('Y年m月d日 H時i分s秒') . 'です。';
 
 // Q4 条件分岐-1 if文
-
+$device = 'windows';
+if ($device === 'windows') {
+    echo '使用OSはwindowsです。';
+}else {
+    if ($device === 'mac') {
+        echo '使用OSはmacです。';
+    }else {
+        echo 'どちらでもありません。';
+    }
+}
 
 // Q5 条件分岐-2 三項演算子
-
+$age = 20;
+$message = ($age >= 18) ? '成人です。' : '未成年です。';
 
 // Q6 配列
-
+$prefecture = [
+    '茨城県', 
+    '群馬県', 
+    '栃木県', 
+    '千葉県', 
+    '東京都', 
+    '神奈川県',
+    '埼玉県'
+];
+echo $prefecture[2] . 'と' . $prefecture[3] . 'は関東地方の都道府県です。'
 
 // Q7 連想配列-1
+$prefecture = [
+    '東京都' => '新宿区', 
+    '神奈川県' => '横浜市', 
+    '千葉県' => '千葉市', 
+    '埼玉県' => 'さいたま市', 
+    '栃木県' => '宇都宮市', 
+    '群馬県' => '前橋市',
+    '茨城県' => '水戸市'
+];
 
+foreach ($prefecture as $city) {
+    echo $city . "\n";
+}
 
 // Q8 連想配列-2
-
+if (array_key_exists('埼玉県', $prefecture)) {
+  echo '埼玉県の県庁所在地は、' . $prefecture['埼玉県'] . 'です。';
+}
 
 // Q9 連想配列-3
+$prefecture = [
+    '東京都' => '新宿区', 
+    '神奈川県' => '横浜市', 
+    '千葉県' => '千葉市', 
+    '埼玉県' => 'さいたま市', 
+    '栃木県' => '宇都宮市', 
+    '群馬県' => '前橋市',
+    '茨城県' => '水戸市',
+    '愛知県' => '名古屋市',
+    '大阪府' => '大阪市'
+];
 
+$kantou = [
+    '東京都' => '新宿区', 
+    '神奈川県' => '横浜市', 
+    '千葉県' => '千葉市', 
+    '埼玉県' => 'さいたま市', 
+    '栃木県' => '宇都宮市', 
+    '群馬県' => '前橋市',
+    '茨城県' => '水戸市'
+];
+
+foreach ( $prefecture as $prefecture => $city) {
+    if (in_array($prefecture, $kantou, true)) {
+        echo $prefecture . 'の県庁所在地は' . $city . 'です。';
+    } else {
+        echo $prefecture . 'は関東地方ではありません。' . "\n";
+    }
+}
 
 // Q10 関数-1
-
+function hello($name)
+{
+    echo $name . 'さん、こんにちは。' . "\n";
+}
+hello('金谷');
+hello('安藤');
 
 // Q11 関数-2
+function calcTaxInPrice($price)
+{
+    return $price * 1.1;
+}
 
+$price = 1000;
+$taxInPrice = calcTaxInPrice($price);
+echo $price . '円の商品の税込価格は' . $taxInPrice . '円です。';
 
 // Q12 関数とif文
-
+function distinguishNum($num)
+{
+    if ($num  % 2 === 0) {
+        return $num . 'は偶数です。';
+    } else {
+        return $num . 'は奇数です。';
+    }
+}
+echo distinguishNum(11) . "\n";
+echo distinguishNum(24) . "\n";
 
 // Q13 関数とswitch文
-
+function evaluateGrade($grade)
+{
+    switch ($grade) {
+        case 'A':
+        case 'B':
+            return '合格です。';
+            break;
+        
+        case 'C':
+            return '合格ですが、追加課題があります。';
+            break;
+        
+        case 'D':
+            return '不合格です。';
+            break;
+        
+        default: 
+            return '判定不明です。講師に問い合わせてください。';
+            break;
+    }
+}
+echo evaluateGrade('A') . "\n";
+echo evaluateGrade('F') . "\n";
 
 ?>
